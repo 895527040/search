46362000a2ce3b8c576844d576b95e99ac0900a8
lin <895527040@qq.com>
2021-06-09 19:10:53 +0800

    3

--- src/main.js
+++ src/main.js
@@ -3,68 +3,66 @@ const $lastLi = $siteList.find('li.last')
 const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)
 const hashMap = xObject || [
-	{ logo: 'A', url: 'https://www.acfun.cn' },
-	{ logo: 'B', url: 'https://www.bilibili.com' },
-	{ logo: 'D', URL: 'https://www.dangdang.com' },
-	{ logo: 'G', URL: 'https://www.google.com' },
-	{ logo: 'G', URL: 'https://www.github.com' },
+  {logo: 'A', url: 'https://www.acfun.cn'},
+  {logo: 'B', url: 'https://www.bilibili.com'}
 ]
 const simplifyUrl = (url) => {
-	return url.replace('https://', '')
-		.replace('http://', '')
-		.replace('www.', '')
-		.replace(/\/.*/, '')
+  return url.replace('https://', '')
+    .replace('http://', '')
+    .replace('www.', '')
+    .replace(/\/.*/, '') // 删除 / 开头的内容
 }
 
 const render = () => {
-	$siteList.find('li:not(.last)').remove()
-	hashMap.forEach((node, index) => {
-		const $li = $(`<li>
-<div class="site">
-<div class="logo">${node.logo}</div>
-<div class = "link">${simplifyUrl(node.url)}</div>
-<div class = "close">
-<svg class = "icon">
-<use xlink:href="#icon-close"></use>
-</svg>
-</div>
-</div>
-</li>`).insertBefore($lastLi)
-		$li.on('click', () => {
-			window.open(node.url)
-		})
-		$li.on('click', '.close', (e) => {
-			e.stopPropagation()
-			hashMap.splice(index, 1)
-			render()
-		})
-	})
+  $siteList.find('li:not(.last)').remove()
+  hashMap.forEach((node, index) => {
+    const $li = $(`<li>
+      <div class="site">
+        <div class="logo">${node.logo}</div>
+        <div class="link">${simplifyUrl(node.url)}</div>
+        <div class="close">
+          <svg class="icon">
+            <use xlink:href="#icon-close"></use>
+          </svg>
+        </div>
+      </div>
+    </li>`).insertBefore($lastLi)
+    $li.on('click', () => {
+      window.open(node.url)
+    })
+    $li.on('click', '.close', (e) => {
+      e.stopPropagation() // 阻止冒泡
+       hashMap.splice(index, 1)
+      render()
+    })
+  })
 }
 
 render()
 
 $('.addButton').on('click', () => {
-	let url = window.prompt('请问您想添加什么网站呀？（๑ `▽´๑)')
-	if (url.indexOf('http') !== 0) {
-		url = 'https://' + url;
-	}
-	console.log(url)
-	hashMap.push({
-		logo: simplifyUrl(url)[0].toUpperCase(),
-		url: url
-	});
-	render();
-});
+  let url = window.prompt(' 请问你要添加的网址是啥？')
+  if (url.indexOf('http') !== 0) {
+    url = 'https://' + url
+  }
+  console.log(url)
+  hashMap.push({
+    logo: simplifyUrl(url)[0].toUpperCase(),
+    url: url
+  })
+  render()
+})
+
 window.onbeforeunload = () => {
-	const string = JSON.stringify(hashMap)
-	localStorage.setItem('x', string)
+  const string = JSON.stringify(hashMap)
+  localStorage.setItem('x', string)
 }
 
 $(document).on('keypress', (e) => {
-	const { key } = e
-	for (let i = 0; i < hashMap.length; i++) {
-		if (hashMap[i].logo.toLowerCase() === key) {
-			window.open(hashMap[i].url)
-		}
-	}
+  const {key} = e
+  for (let i = 0; i < hashMap.length; i++) {
+    if (hashMap[i].logo.toLowerCase() === key) {
+      window.open(hashMap[i].url)
+    }
+  }
 })
